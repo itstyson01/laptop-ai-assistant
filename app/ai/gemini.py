ecb2bad3bfad1e9436b226820ff16544151f762a
Amandeep Yadav <amandeepdon200319@gmail.com>
2026-08-17 21:34:23 +0530

    feat: integrate Gemini AI

---
+++ app/ai/gemini.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+chat = client.chats.create(
+    model="gemini-2.5-flash"
+)
+
+
+def ask_gemini(prompt: str) -> str:
+    response = chat.send_message(prompt)
+
+    return response.text
---
+++ app/main.py
@@ -0,0 +1,6 @@
+from ai.gemini import ask_gemini
+
+
+response = ask_gemini("what is capital of India")
+
+print(response)
